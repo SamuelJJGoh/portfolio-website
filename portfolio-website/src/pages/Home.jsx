4f9724294f507a9bcd2aea299719ae6fe7c4cbbe
Samuel Goh Jun Je <samueljjgoh@gmail.com>
2025-12-07 23:06:23 +0000

    Add a navbar and a hero section

--- portfolio-website/src/pages/Home.jsx
+++ portfolio-website/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { HeroSection } from "../components/HeroSection";
 
 export const Home = () => {
 
@@ -8,11 +10,17 @@ export const Home = () => {
             <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
                 {/* Theme Toggle */}
                 <ThemeToggle />
+
                 {/* Background Effects */}
                 <StarBackground />
+
                 {/* Navbar */}
+                <Navbar />
 
                 {/* Main Content */}
+                <main>
+                    <HeroSection />
+                </main>
 
                 {/* Footer */}
             </div>
