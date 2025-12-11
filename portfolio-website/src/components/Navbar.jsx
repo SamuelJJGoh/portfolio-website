931ffafcec24e76d43e15c13827bf1168139a474
Samuel Goh Jun Je <samueljjgoh@gmail.com>
2025-12-11 02:12:30 +0000

    Add a Not Found Page

--- portfolio-website/src/components/Navbar.jsx
+++ portfolio-website/src/components/Navbar.jsx
@@ -54,7 +54,7 @@ export const Navbar = () => {
                     >
                         {isMenuOpen ? <X size={24} /> : <Menu size={24}/>}
                     </button>
-                    <div className={cn("fixed inset-0 h-screen w-screen bg-background/60 backdrop-blur-md z-30 flex flex-col items-center justify-center",
+                    <div className={cn("fixed inset-0 h-screen w-screen bg-background/90 backdrop-blur-md z-30 flex flex-col items-center justify-center",
                                         "transition-all duration-300 md:hidden",
                                         isMenuOpen ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none",
                     )}>
--- portfolio-website/src/pages/NotFound.jsx
+++ portfolio-website/src/pages/NotFound.jsx
@@ -1,8 +1,38 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+import { StarBackground } from "../components/StarBackground";
+import { Navbar } from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import { House, ArrowLeft } from 'lucide-react';
+
+
 export const NotFound = () => {
 
     return(
         <>
-            Not Found
+            <div className="min-h-screen bg-background text-foreground overflow-x-hidden flex flex-col">
+                {/* Theme Toggle */}
+                <ThemeToggle />
+
+                {/* Background Effects */}
+                <StarBackground />
+
+                {/* Navbar */}
+                <Navbar />
+
+                {/* Main Content */}
+                <main className="flex-1 flex flex-col items-center justify-center text-center px-4">
+                    <div className="space-y-4">
+                        <h1 className="text-6xl font-bold text-primary">404</h1>
+                        <p className="text-muted-foreground text-3xl">Page Not Found</p>
+                        <a href="/" className="cosmic-button mt-4 inline-flex items-center justify-center gap-3">
+                            <House size={16}/>Go Back Home <ArrowLeft size={16}/>
+                        </a>
+                    </div>
+                </main>
+
+                {/* Footer */}
+                <Footer />
+            </div>
         </>
     );
 }
