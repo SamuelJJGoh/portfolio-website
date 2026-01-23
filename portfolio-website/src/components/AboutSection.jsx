35d465eb96061486bac462248aa521e61d032344
Samuel Goh Jun Je <samueljjgoh@gmail.com>
2026-01-23 04:05:09 +0000

    Fix bug

--- portfolio-website/src/components/AboutSection.jsx
+++ portfolio-website/src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
                             </a>
                             <a href="/SamuelGohCV.pdf" target="_blank" rel="noreferrer" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300 flex items-center justify-center gap-2">
                                 <FaRegFilePdf className="h-5 w-5" />
-                                Download CV
+                                View CV
                             </a>
                         </div>
                     </div>
